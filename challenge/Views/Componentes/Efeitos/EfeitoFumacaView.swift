7c7a6c7d3029dc80f6295eeac6e2e815e5d331b3
felipecardoso32 <felipepro1@hotmail.com>
2026-05-27 14:23:06 -0300

    Criação de efeitos.

--- challenge/Views/Componentes/Efeitos/EfeitoFumacaView.swift
+++ challenge/Views/Componentes/Efeitos/EfeitoFumacaView.swift
@@ -7,12 +7,35 @@
 
 import SwiftUI
 
-struct EfeitoFumacaView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+
+struct efeitoFumaca: Identifiable{
+    let id = UUID()
+    var x: CGFloat
 }
 
+struct EfeitoFumacaView: View {
+    
+    @State private var offsetY: CGFloat = 0
+    @State private var opacity: Double = 1
+    
+    var body: some View {
+        Image("nuvem cinza 1")
+            .rotationEffect(.degrees([90, 180.0].randomElement() ?? 0.0))
+            .opacity(opacity)
+            .offset(y: offsetY)
+            //.rotationEffect(.degrees([0.0, 180.0].randomElement() ?? 0.0))
+            .saturation(0.6)
+            .onAppear {
+                withAnimation(.easeOut(duration: 2.5)){
+                    offsetY = -120
+                    opacity = 0
+                }
+            }
+        
+    }
+}
+/*
 #Preview {
     EfeitoFumacaView()
 }
+*/
