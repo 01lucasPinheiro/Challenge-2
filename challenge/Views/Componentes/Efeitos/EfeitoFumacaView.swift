b743bd9be4109c68a39e93fa6cd5ac239a948c16
felipecardoso32 <felipepro1@hotmail.com>
2026-05-27 13:34:07 -0300

    Inicio de criação de tela de efeito e upload de imagens de rostos

---
+++ challenge/Views/Componentes/Efeitos/EfeitoFumacaView.swift
@@ -0,0 +1,18 @@
+//
+//  EfeitoFumacaView.swift
+//  teste
+//
+//  Created by Felipe Colares Cardoso on 27/05/26.
+//
+
+import SwiftUI
+
+struct EfeitoFumacaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EfeitoFumacaView()
+}
